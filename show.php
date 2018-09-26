99e490152027743811130f093ad24f64b87e0a0e
qiaogg <43558337+qiaogg@users.noreply.github.com>
2018-09-26 23:21:04 +0800

    第一次提交
    第一次提交

---
+++ show.php
@@ -0,0 +1,22 @@
+﻿<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>show | 博客</title>
+</head>
+<body>
+  <?php        
+    mysql_connect('127.0.0.1','root','root') or die('can`t work');
+    mysql_query("SET NAMES utf8");    
+    mysql_select_db('blog');
+
+    $sql = 'select * from posts where id =1 ' ;
+    $query = mysql_query($sql);
+    $post = mysql_fetch_object($query);
+  ?>
+
+  <h1><?php echo $post->id; ?> : <?php echo $post->title; ?>  </h1>
+  <p><?php echo $post->body; ?></p>
+
+</body>
+</html>
